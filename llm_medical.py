bd8e34fc6b48f54a4a18d1a579bd76aa788670ea
NikitaBisht001 <118534619+NikitaBisht001@users.noreply.github.com>
2024-08-25 18:43:46 +0530

    Update llm_medical.py

--- llm_medical.py
+++ llm_medical.py
@@ -69,3 +69,31 @@ prompt = "how to cure feaver?"
 pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
 result = pipe(f"<s>[INST] {prompt} [/INST]")
 print(result[0]['generated_text'])
+
+
+
+# for chatbot we use Chainlit so the code for that will be
+@cl.on_chat_start
+async def start():
+    chain = qa_bot()
+    msg = cl.Message(content="Starting the bot...")
+    await msg.send()
+    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
+    await msg.update()
+    cl.user_session.set("chain", chain)
+
+@cl.on_message
+async def main(message: cl.Message):
+    chain = cl.user_session.get("chain") 
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message.content, callbacks=[cb])
+    answer = res["result"]
+    sources = res["source_documents"]
+    if sources:
+        answer += f"\nSources:" + str(sources)
+    else:
+        answer += "\nNo sources found"
+    await cl.Message(content=answer).send()
